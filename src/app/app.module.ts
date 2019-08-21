319284428535e9d84d638b9cba777c1eb3a07d34
tambamoussa <53384505+tambamoussa@users.noreply.github.com>
2019-08-21 17:49:52 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProduitComponent } from './produit/produit.component';
+import {ProduitMockService} from './produit/produit.mock.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProduitComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [ProduitMockService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
